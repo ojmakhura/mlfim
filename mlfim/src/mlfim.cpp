075ef5d5525f840618b49ccd15ef09342cdef520
Onalenna Junior Makhura <ojmakhura@systemsjr.com>
2017-06-16 15:52:43 +0100

    got it working

--- mlfim/src/mlfim.cpp
+++ mlfim/src/mlfim.cpp
@@ -28,9 +28,30 @@ using namespace cv::ximgproc::segmentation;
 
 int main(int argc, char** argv) {
 	ocl::setUseOpenCL(true);
-	Mat frame;
+	framed f;
+    Ptr<Feature2D> detector = SURF::create(500);
 
-    Ptr<Feature2D> detector = SURF::create(1500);
+    String name = argv[1];
+    f.frame = imread(name);
+    detector->detectAndCompute(f.frame, Mat(), f.keypoints, f.descriptors);
+
+    int m = atoi(argv[3]);
+	hdbscan scan(f.descriptors, _EUCLIDEAN, m, m);
+	scan.run();
+	f.labels = scan.getClusterLabels();
+	set<int> lset(f.labels.begin(), f.labels.end());
+	mapKeyPoints(f);
+	printf("lset has %lu\n", lset.size());
+	printf("f.clusterKeypointIdx has %lu\n", f.clusterKeypointIdx.size());
+
+	for(map<int, vector<KeyPoint>>::iterator it = f.clusterKeyPoints.begin(); it != f.clusterKeyPoints.end(); ++it){
+		Mat m = drawKeyPoints(f.frame, it->second, Scalar(0, 0, 255), -1);
+		String s = "frame_keypoints_";
+		string s1 = to_string(it->first);
+
+		s += s1.c_str();
+		printImage(argv[2], 1, s, m);
+	}
 
 
 	return 0;
--- python/feature_plot.py
+++ python/feature_plot.py
@@ -3,5 +3,77 @@
 Junior
 
 Plotting local features as clusters
+
+===================================
+Demo of HDBSCAN clustering algorithm
+===================================
+Finds a clustering that has the greatest stability over a range
+of epsilon values for standard DBSCAN. This allows clusterings
+of different densities unlike DBSCAN.
 """
+print(__doc__)
+
 import cv2
+
+import numpy as np
+
+from hdbscan import HDBSCAN
+from sklearn import metrics
+
+import time
+
+def getClusterIdx(labels, label):
+    lst = []
+    
+    for i in range(len(labels)):
+        if labels[i] == label:
+            lst.append(i)
+            
+    return lst
+
+def getKeypoints(keypoints, idx):
+    kps = []
+    for i in idx:
+        kps.append(keypoints[i])
+    return kps
+
+def decomposeDescriptors(desc):
+    shape = desc.shape
+    dec = [[], []]
+    for i in range(shape[0]):
+        x, y = 0.0, 0.0
+        row = desc[i]
+        for j in range(0, shape[1], 2):
+            x = x + row[j]
+            y = y + row[j+1]
+        dec[0].append(x)
+        dec[1].append(y)
+    return dec
+    
+def distance(desc):
+    ground = desc[1]
+    dist = []
+    for row in desc:
+        d = np.linalg.norm(row - ground)
+        dist.append(d)
+    return dist
+
+def showCluster(img, label):
+    idx = getClusterIdx(labels1, label)
+    kps = getKeypoints(keypoints, idx)
+    img2 = cv2.drawKeypoints(image,kps,None,(255,0,0), cv2.DRAW_MATCHES_FLAGS_)
+    plt.imshow(img2)
+    plt.show()
+    
+image = cv2.imread('/home/ojmakh/programming/phd/data/vocount/test/1 frame.jpg')
+surf = cv2.xfeatures2d.SURF_create(500)
+keypoints, descriptors = surf.detectAndCompute(image, None)
+
+##############################################################################
+# Compute DBSCAN
+hdb_t1 = time.time()
+hdb = HDBSCAN(min_cluster_size=4).fit(descriptors.astype('double'))
+labels1 = hdb.labels_
+hdb_elapsed_time = time.time() - hdb_t1
+
+lset1 = set(labels1)
